b506d648475a090236815a31ea9c3643338bb496
CMF-chenmengfei <15239905718@163.com>
2023-04-26 16:46:13 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,18 @@ import { Provider } from "react-redux";
 // react-redux  仓库
 import { store } from "./store";
 
-// 引入css样式
-import "antd/dist/reset.css";
-import "./index.css";
-import "./styles/flex.scss";
-
 import "dayjs/locale/zh-cn";
 import locale from "antd/locale/zh_CN";
 import { ConfigProvider } from "antd";
 
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
+
+// 引入css样式
+import "antd/dist/reset.css";
+import "./index.css";
+import "./styles/flex.scss";
+
 import App from "./App";
 
 NProgress.configure({ showSpinner: false });
